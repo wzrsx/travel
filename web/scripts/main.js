da75a192221bd33660884c391b4c34666811b7cb
SergeyRehub <serzh.rybakov.06@gmail.com>
2025-03-02 14:26:40 +0300

    JsReg

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,6 +1,7 @@
 const signInDialog = document.getElementById('signInDialog');
 const registrationDialog = document.getElementById('registrationDialog');
 const blurDiv = document.getElementById('blurDiv');
+// const IP = process.env.IP; перемнные среды
 
 function openSignInDialog() {
     if (registrationDialog.open) {
@@ -12,7 +13,7 @@ function openSignInDialog() {
     }
 }
 
-function Authorize(){
+function authorize(){
     const email = document.getElementById('signInEmailInput').value;
     const password = document.getElementById('signInPasswordInput').value;
 
@@ -49,6 +50,49 @@ function openRegistrationDialog() {
         blurDiv.classList.add('blur'); // Добавляем класс размытия только если диалог не открыт
         registrationDialog.showModal(); // Открываем диалог регистрации
     }
+}
+function registration(){
+    const email = document.getElementById('registrationEmailInput').value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const password = document.getElementById('registrationPasswordInput').value;
+    const repeat_password = document.getElementById('registrationPasswordInputRepeat').value;
+    const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    if (!emailRegex.test(email)){
+        //condition returning error with incorrect email format
+        return;
+    }
+    if (!password.length >= 5){
+        //condition returning error size of password
+        return;
+    }
+    if (!specialCharRegex.test(password)){
+        //condition returning error special simbol absence
+        return;
+    }
+    if (password != repeat_password) {
+        //condition returning error not matching passwords 
+        return;
+    }
+    const data = {
+        email: email,
+        password: password
+    };
+    fetch("http://localhost:8080/registration"),{
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    .then (response =>{
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        location.reload();
+    })
+    .catch ((error)=>{
+        console.log('Error: ', error);
+    })
 }
 
 // Закрытие диалогов и удаление размытия
